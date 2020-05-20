6f5b71e104718d6bac5b8693b188c8de49f5c980
Sanjeev Rohila <s.rohila@f5.com>
2020-05-20 12:12:10 +0530

    flake8

--- sample.py
+++ sample.py
@@ -1,12 +1,15 @@
-from flask import Flask, render_template,request
+from flask import (
+    Flask,
+    render_template)
 
 
 app = Flask(__name__)
 
+
 @app.route("/")
-#@ldap.login_required
 def index():
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
